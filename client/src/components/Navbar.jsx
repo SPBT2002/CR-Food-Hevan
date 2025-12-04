e1a39cfd70a15520845151b139042991443393d9
Supun Piyumal <supunthennakoon27@gmail.com>
2025-12-04 23:24:38 +0530

    Include navigation links and improve structure

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
-const Navbar = () => {
+const Navbar = ({containerStyles, setMenuOpened}) => {
+    const navLinks = [
+      { path: '/', name: 'Home' },
+      { path: '/menu', name: 'Menu' },
+      { path: '/blog', name: 'Blog' },
+      { path: '/contact', name: 'Contact' },
+    ]
+
   return (
-    <div>
-      Nav
-    </div>
+    <nav className={containerStyles}>
+      {navLinks.map((link) => (
+        <NavLink onClick={() => setMenuOpened && setMenuOpened(false)} key={link.title} to={link.path} className={({isActive}) => '${isActive ? active-link" : ""} px-3 py-2 rounded-full uppercase text-sm font-bold'}>
+
+        </NavLink>
+      ))}
+    </nav> 
   )
 }
 
