b80579ab27f1306558604463570ce6aefd1e90a8
Supun Piyumal <supunthennakoon27@gmail.com>
2025-12-06 23:36:03 +0530

    Add Hero component to Home page for enhanced user experience

---
+++ client/src/components/Hero.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section>
+        <div>
+            <div>
+                <div>
+                    <h3>Welcome to Candriya Food Hevan</h3>
+                    <h2>
+                        <span className='text-solidOne'>Get More</span><span className='text-solidTwo'>for Less - 25% off!</span>
+                    </h2>
+                    <h1>
+                        on Rice & Curries
+                    </h1>
+                    <div>
+                        <h3>Starting From</h3>
+                        <span>$</span>04.
+                        <span className='text-2xl'>99</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Hero
