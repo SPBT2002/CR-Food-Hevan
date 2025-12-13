cb5d7d8ccade52e34bc0ab8d51559ed5445ad7c7
Supun Piyumal <supunthennakoon27@gmail.com>
2025-12-13 20:05:58 +0530

    Refactor Item component styles and improve order information layout

--- client/src/components/Item.jsx
+++ client/src/components/Item.jsx
@@ -6,28 +6,28 @@ const Item = ({product}) => {
   const currency = '$';
 
   return (
-    <div className='relativemt-24 group'>
+    <div className='relative mt-24 group'>
       {/*Photo*/}
-      <div className='mx-auto rounded-full absolute left-0 right-0 -top-21 h-[177px] w-[177px]'>
+      <div className='mx-auto rounded-full absolute left-0 right-0 -top-20 h-[177px] w-[177px]'>
         <img 
           src={product.images[0]} 
           alt="productImg" 
           height={177} 
           width={177}
-          className='absolute inst-0 h-full w-full object-cover opacity-100 group-hover:opacity-0 drop-shadow-md' 
+          className='absolute inset-0 h-full w-full object-cover opacity-100 group-hover:opacity-0 drop-shadow-md' 
         />
         <img 
           src={product.images[1] ? product.images[1] : product.images[0]} 
           alt="productImg" 
           height={177} 
           width={177}
-          className='absolute inst-0 h-full w-full object-cover opacity-0 group-hover:opacity-100 drop-shadow-md' 
+          className='absolute inset-0 h-full w-full object-cover opacity-0 group-hover:opacity-100 drop-shadow-md' 
         />
       </div>
       {/*Info*/}
-      <div className='rounded-4xl bg-primarypt-20 overflow-hidden'>
+      <div className='rounded-4xl bg-primary pt-20 overflow-hidden'>
         {/* Title & Description */}
-        <div className='p-3'>
+        <div className='p-3 pt-3'>
           <h4 className='text-lg line-clamp-1 mb-1'>{product.title}</h4>
           <div className='flex items-start justify-between pb-1'>
             <h5 className='mb-1'>{product.category}</h5>
@@ -51,7 +51,7 @@ const Item = ({product}) => {
                 onClick={()=> setSize(item)}
                 className={`${
                   item === size ? 'btn-light' : 'btn-outline'
-                } rounded h-6 w-6 p-2 text-x5 flexCenter`}
+                } rounded h-6 w-6 p-2 text-xs flexCenter`}
               >
                   {item}
               </button>
@@ -60,20 +60,20 @@ const Item = ({product}) => {
           <h4 className='text-solidOne'>{currency}{product.price[size]}</h4>
         </div>
         {/* Order Info & Button */}
-        <div className='flexBetween rounded-xl pl-5 text-[13px] font-semibold'>
+        <div className='flexBetween rounded-xl pl-5 pr-3 text-[13px] font-semibold'>
           <div className='flexStart gap-5'>
             <div className='flex flex-col gap-1 relative bottom-1.5'>
               <h5>Prep</h5>
-              <p className='text-x5'>5min</p>
+              <p className='text-xs'>5min</p>
             </div>
             <hr className="h-8 w-[1px] bg-tertiary/10 border-none"/>
             <div className='flex flex-col gap-1 relative bottom-1.5'>
               <h5>Cook</h5>
-              <p className='text-x5'>20min</p>
+              <p className='text-xs'>20min</p>
             </div>
           </div>
           <div className='flex flex-col gap-1'>
-            <button>
+            <button className='btn-solid rounded-xl p-3'>
               <img src={assets.cartAdd} alt="" width={20}/>
             </button>
           </div>
