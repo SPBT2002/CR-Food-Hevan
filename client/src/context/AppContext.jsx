8c4ee22770a1447fb4e5498b821d4b2803fe47a2
Supun Piyumal <supunthennakoon27@gmail.com>
2025-12-28 21:37:27 +0530

    Add cartItems state and addCart function to manage cart operations

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,4 +1,5 @@
 import React, { createContext, useContext, useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { useUser } from '@clerk/clerk-react'
 import { dummyProducts } from '../assets/data';
 
@@ -7,14 +8,21 @@ const AppContext = createContext()
 export const AppContextProvider = ({children}) => {
     const [products, setProducts] = useState([]);
     const [searchQuery,setsearchQuery] = useState("")
+    const [cartItems, setCartItems] = useState([])
     const currency = import.meta.env.VITE_CURRENCY
     const delivery_charge = 10
+    const navigate = useNavigate()
     //Clerk
     const {user} = useUser()
     
     const fetchProducts = () => {
         setProducts(dummyProducts);
     };
+
+    //Add Products to  Cart
+    const addCart = (itemId, size) => {
+        if(!size) return toast
+    }
 
     useEffect(() => {
         fetchProducts();
