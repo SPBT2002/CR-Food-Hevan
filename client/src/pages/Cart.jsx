5a3a5d68c9abc6f9d5f1a3e550fd3c33d2f5ddf9
Supun Piyumal <supunthennakoon27@gmail.com>
2025-12-29 22:59:07 +0530

    Enhance Cart component with cart data management and quantity update functions

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -5,7 +5,35 @@ import { useAppContext } from '../context/AppContext'
 import { assets } from '../assets/data'
 
 const Cart = () => {
-  const {navigate, products, currency, cartItems} = useAppContext()
+  const {navigate, products, currency, cartItems, updateQuantity} = useAppContext()
+  const [cartData, setcartData] = useState([])
+
+  useEffect(() => {
+    if(products.length > 0) {
+      const tempData = []
+      for(const itemId in cartItems) {
+        for (const size in cartItems[itemId]) {
+          if(cartItems[itemId][size] > 0) {
+            tempData.push({
+              _id: itemId,
+              size: size
+            })
+          }
+        }
+       }
+      setcartData(tempData)
+     }
+  }, [products, cartItems])
+
+  const increment = (id, size) => {
+    const currentQuantity = cartItems[id][size]
+    updateQuantity(id, size, currentQuantity + 1)
+  }
+
+  const decrement = (id, size) => {
+    const currentQuantity = cartItems[id][size]
+    updateQuantity(id, size, currentQuantity + 1)
+  }
 
   return (
     <div>Cart</div>
